cabaec2739ef75930d94758c8ef2df3b0a499a1e
Rammun <russslllan@mail.ru>
2016-03-02 02:30:56 +0300

    Добавил чистый проект MVC

---
+++ VK-Monitor.WebMVC/VK-Monitor.WebMVC/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace VK_Monitor.WebMVC
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ VK-Monitor.WebMVC/VK-Monitor.WebMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="VK_Monitor.WebMVC.MvcApplication" Language="C#" %>
