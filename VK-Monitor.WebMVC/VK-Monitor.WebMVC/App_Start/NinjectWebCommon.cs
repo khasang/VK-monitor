751ca045425f0f13b354f1803aea4bb6713e7a50
Rammun <russslllan@mail.ru>
2016-03-06 01:14:55 +0300

    Добавил регистрацию зависимостей

--- VK-Monitor.WebMVC/VK-Monitor.WebMVC/App_Start/NinjectWebCommon.cs
+++ VK-Monitor.WebMVC/VK-Monitor.WebMVC/App_Start/NinjectWebCommon.cs
@@ -10,6 +10,7 @@ namespace VK_Monitor.WebMVC.App_Start
 
     using Ninject;
     using Ninject.Web.Common;
+    using System.Web.Mvc;
 
     public static class NinjectWebCommon 
     {
@@ -61,6 +62,7 @@ namespace VK_Monitor.WebMVC.App_Start
         /// <param name="kernel">The kernel.</param>
         private static void RegisterServices(IKernel kernel)
         {
+            DependencyResolver.SetResolver(new VK_Monitor.WebMVC.Infrastructure.NinjectDependencyResolver(kernel));
         }        
     }
 }
